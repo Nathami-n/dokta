78c1f3ca69dcc94fe434b6db31543b91b1d1d96d
Nathan George <georgenathan010@gmail.com>
2024-03-21 02:44:06 +0300

    added the searchbar

--- app/components/NavBar/SearchBar.tsx
+++ app/components/NavBar/SearchBar.tsx
@@ -1,30 +1,54 @@
 'use client'
-
+import {CiSearch} from 'react-icons/ci'
 interface SearchBarProps {
 
 }
 
 const SearchBar = () => {
   return (
+    <div className='
+    
+    '>
    <form
     action=""
     className="
     border
-    rounded-lg
-    
+    hover:shadow-md
+    transition
+    cursor-pointer
+    rounded-xl
+    p-1
+    relative
+    w-[200px]
     "
    >
     <input 
     type="text"
     className="
     outline-none
-    border
     p-3
     w-full
     "
     placeholder="search..."
      />
+     <CiSearch 
+     type='submit'
+     className="
+     bg-blue-500
+     text-white
+     rounded-full
+     transition
+     hover:bg-rose-500
+     cursor-pointer
+     p-1
+     absolute
+     top-2
+     right-3
+      "
+     size={35}
+     />
    </form>
+   </div>
   )
 }
 
