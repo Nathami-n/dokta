2fb6d6876da33393de63b8fdbdf1581e1d95ef72
Nathan George <georgenathan010@gmail.com>
2024-03-31 22:43:37 +0300

    Add: shadcn

--- app/create/[id]/location/page.tsx
+++ app/create/[id]/location/page.tsx
@@ -1,9 +1,50 @@
-
+import {
+  Select, 
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select'
 
 const Location = () => {
   return (
-    <div>Location</div>
-  )
+    <>
+    <div 
+    className="
+    mx-auto
+    max-w-7xl
+    "
+    >
+      <h2 className="
+      text-3xl
+      font-semibold
+      tracking-tight
+      transition-colors
+      text-center
+      mt-8
+      mb-8
+      "> Where are you located?
+      </h2>
+      <form>
+      <div className="
+      max-w-7xl
+      mx-auto
+      ">
+        <div className="mb-5 p-4">
+          <Select required>
+          <SelectTrigger 
+          className='
+            max-w-5xl
+            mx-auto
+          '>
+            <SelectValue placeholder='Select Country'/>
+          </SelectTrigger>
+          </Select>
+
+        </div>
+      </div>
+      </form>
+    </div>
+    </>
+  ) 
 }
 
 export default Location
